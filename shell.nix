a0817a56049ca474c42dbf5539dc636840f82c12
Fabian Kirchner <kirchner@posteo.de>
2022-07-09 14:45:38 +0200

    chore: clean up repo

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    elmPackages.elm
+    elmPackages.elm-test
+    elmPackages.elm-format
+    elmPackages.elm-doc-preview
+    elmPackages.elm-language-server
+    expect
+    inotify-tools
+  ];
+}
